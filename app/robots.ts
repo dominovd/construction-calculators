96537d9909d2ac3f2dcf95ad53b4401d8cb984fd
Denis <denis.dominov@unitedtech.ai>
2026-05-03 12:53:32 +0300

    fix: remove Disallow /contact from robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/contact"],
     },
     sitemap: "https://easybuildcalc.com/sitemap.xml",
   };
